4746a826abf33a88ac69835d9f6095dcf0368a16
sheweijie <sheweijie@example.com>
2025-12-28 18:31:08 +0800

    feat: add recent tools feature to command palette
    - Add recent tools display when search is empty
    - Prioritize recent tools over all tools when available
    - Extract actionOptions to eliminate code duplication
    - Fix Github icon name to use correct lucide-vue-next import

--- src/modules/command-palette/command-palette.store.ts
+++ src/modules/command-palette/command-palette.store.ts
@@ -1,11 +1,7 @@
 import { chain, take } from 'lodash'
 import { defineStore } from 'pinia'
 import { useI18n } from 'vue-i18n'
-import BugIcon from '~icons/mdi/bug-outline'
-import GithubIcon from '~icons/mdi/github'
-
-import InfoIcon from '~icons/mdi/information-outline'
-import SunIcon from '~icons/mdi/white-balance-sunny'
+import { Bug, Github, Info, Sun, Moon } from 'lucide-vue-next'
 import { useFuzzySearch } from '@/composable/fuzzySearch'
 import { useStyleStore } from '@/stores/style.store'
 import { useToolStore } from '@/tools/tools.store'
@@ -14,55 +10,82 @@ export const useCommandPaletteStore = defineStore('command-palette', () => {
   const { t } = useI18n();
   const toolStore = useToolStore();
   const styleStore = useStyleStore();
-  const _router = useRouter();
   const searchPrompt = ref('');
 
-  const searchOptions = computed(() => {
-    const toolsOptions = toolStore.tools.map(tool => ({
+  // Action options that are always shown
+  const actionOptions = computed(() => [
+    {
+      name: t('commandPalette.actions.toggleDarkMode'),
+      description: t('commandPalette.actions.toggleDarkModeDescription'),
+      action: () => styleStore.toggleDark(),
+      icon: styleStore.isDarkTheme ? Sun : Moon,
+      category: t('commandPalette.categories.actions'),
+      keywords: t('commandPalette.keywords.toggleDarkMode'),
+    },
+    {
+      name: t('commandPalette.external.githubRepository'),
+      href: 'https://github.com/willjayyyy/next-tools',
+      category: t('commandPalette.categories.external'),
+      description: t('commandPalette.external.githubRepositoryDescription'),
+      keywords: t('commandPalette.keywords.githubRepository'),
+      icon: Github,
+    },
+    {
+      name: t('commandPalette.actions.reportBug'),
+      description: t('commandPalette.actions.reportBugDescription'),
+      href: 'https://github.com/willjayyyy/next-tools/issues/new/choose',
+      category: t('commandPalette.categories.actions'),
+      keywords: t('commandPalette.keywords.reportBug'),
+      icon: Bug,
+    },
+    {
+      name: t('commandPalette.pages.about'),
+      description: t('commandPalette.pages.aboutDescription'),
+      to: '/about',
+      category: t('commandPalette.categories.pages'),
+      keywords: t('commandPalette.keywords.about'),
+      icon: Info,
+    },
+  ])
+
+  const allToolsOptions = computed(() =>
+    toolStore.tools.map(tool => ({
       ...tool,
       keywords: toValue(tool.keywords),
       name: toValue(tool.name),
       description: toValue(tool.description),
       to: tool.path,
       toolCategory: tool.category,
-      category: 'Tools',
-    }));
+      category: t('commandPalette.categories.tools'),
+    }))
+  )
 
-    return [
-      ...toolsOptions,
-      {
-        name: t('commandPalette.actions.toggleDarkMode'),
-        description: t('commandPalette.actions.toggleDarkModeDescription'),
-        action: () => styleStore.toggleDark(),
-        icon: SunIcon,
-        category: t('commandPalette.categories.actions'),
-        keywords: t('commandPalette.keywords.toggleDarkMode'),
-      },
-      {
-        name: t('commandPalette.external.githubRepository'),
-        href: 'https://github.com/willjayyyy/next-tools',
-        category: t('commandPalette.categories.external'),
-        description: t('commandPalette.external.githubRepositoryDescription'),
-        keywords: t('commandPalette.keywords.githubRepository'),
-        icon: GithubIcon,
-      },
-      {
-        name: t('commandPalette.actions.reportBug'),
-        description: t('commandPalette.actions.reportBugDescription'),
-        href: 'https://github.com/willjayyyy/next-tools/issues/new/choose',
-        category: t('commandPalette.categories.actions'),
-        keywords: t('commandPalette.keywords.reportBug'),
-        icon: BugIcon,
-      },
-      {
-        name: t('commandPalette.pages.about'),
-        description: t('commandPalette.pages.aboutDescription'),
-        to: '/about',
-        category: t('commandPalette.categories.pages'),
-        keywords: t('commandPalette.keywords.about'),
-        icon: InfoIcon,
-      },
-    ];
+  const searchOptions = computed(() => {
+    const isSearchEmpty = !searchPrompt.value.trim()
+
+    if (isSearchEmpty) {
+      // 搜索为空时：优先显示最近工具，如果没有则显示所有工具
+      if (toolStore.recentTools.length > 0) {
+        // 有最近工具时：只显示最近工具 + actions
+        const recentToolsOptions = toolStore.recentTools.map(tool => ({
+          ...tool,
+          keywords: toValue(tool.keywords),
+          name: toValue(tool.name),
+          description: toValue(tool.description),
+          to: tool.path,
+          toolCategory: tool.category,
+          category: t('commandPalette.categories.recentTools', 'Recent Tools'),
+        }))
+
+        return [...recentToolsOptions, ...actionOptions.value]
+      } else {
+        // 没有最近工具时：显示所有工具（按分类分组）+ actions
+        return [...allToolsOptions.value, ...actionOptions.value]
+      }
+    } else {
+      // 有搜索内容时：只显示所有工具（按分类分组）+ actions
+      return [...allToolsOptions.value, ...actionOptions.value]
+    }
   })
 
   const { searchResult } = useFuzzySearch({
